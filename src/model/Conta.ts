4a346076b31b488234572494529f46f14ee55224
Diego Santos <106088657+dizerdev@users.noreply.github.com>
2025-12-16 23:30:09 +0000

    feat: add `TransferenciaValor` and `Recibo` models ans tests

--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -1,25 +1,20 @@
 import { NegocioErro } from "../error/NegocioErro";
+import { NumeroConta } from "./NumeroConta";
 
 export class Conta {
-  private _numero: string;
+  private _numeroConta: NumeroConta;
   private _saldo: number;
 
-  private validarValor(valor: number): void {
-    if (valor <= 0)
-      throw new NegocioErro("valor não pode ser igual ou menor que zero");
-  }
-
   public constructor(numero: string, saldo: number) {
-    this._numero = numero;
+    this._numeroConta = new NumeroConta(numero);
     this._saldo = saldo;
-  }
-
-  public get saldo(): number {
-    return this._saldo;
   }
 
   public sacar(valor: number): void {
     this.validarValor(valor);
+
+    if (valor > this._saldo)
+      throw new NegocioErro("saldo indisponível para operação");
 
     this._saldo -= valor;
   }
@@ -28,5 +23,18 @@ export class Conta {
     this.validarValor(valor);
 
     this._saldo += valor;
+  }
+
+  public get saldo(): number {
+    return this._saldo;
+  }
+
+  public get numero(): string {
+    return this._numeroConta.numero;
+  }
+
+  private validarValor(valor: number): void {
+    if (valor <= 0)
+      throw new NegocioErro("valor não pode ser igual ou menor que zero");
   }
 }
---
+++ src/model/NumeroConta.ts
@@ -0,0 +1,25 @@
+import { NegocioErro } from "../error/NegocioErro";
+
+export class NumeroConta {
+  private _numero: string;
+
+  private validar(numero: string): void {
+    if (!this.temSeisDigitos(numero)) {
+      throw new NegocioErro("número de conta inválida");
+    }
+  }
+
+  private temSeisDigitos(numero: string): boolean {
+    const regExp: RegExp = /^\d{6}$/;
+    return regExp.test(numero);
+  }
+
+  public constructor(numero: string) {
+    this.validar(numero);
+    this._numero = numero;
+  }
+
+  public get numero(): string {
+    return this._numero;
+  }
+}
