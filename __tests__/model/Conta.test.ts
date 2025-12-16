25cadaf20c7012f888f09a6c95d4d84f15c15204
Diego Santos <106088657+dizerdev@users.noreply.github.com>
2025-12-16 22:55:38 +0000

    feat: include validations in `Conta` class and make tests

--- __tests__/model/Conta.test.ts
+++ __tests__/model/Conta.test.ts
@@ -1,8 +1,29 @@
 import { Conta } from "../../src/model/Conta";
 
 describe("Conta", () => {
-  test("saque sucesso", async () => {
-    const conta: Conta = new Conta();
-    expect(conta.sacar()).toBe(1000);
+  test("sacar com sucesso", async () => {
+    const conta: Conta = new Conta("123456", 5000.0);
+    conta.sacar(200.0);
+    expect(conta.saldo).toBe(4800.0);
+  });
+
+  test("depositar com sucesso", async () => {
+    const conta: Conta = new Conta("123456", 5000.0);
+    conta.depositar(200.0);
+    expect(conta.saldo).toBe(5200.0);
+  });
+
+  test("sacar com valor zerado", async () => {
+    const conta: Conta = new Conta("123456", 5000.0);
+    expect(() => {
+      conta.sacar(0);
+    }).toThrow("valor não pode ser igual ou menor que zero");
+  });
+
+  test("depositar com valor zerado", async () => {
+    const conta: Conta = new Conta("123456", 5000.0);
+    expect(() => {
+      conta.depositar(0);
+    }).toThrow("valor não pode ser igual ou menor que zero");
   });
 });
--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -1,5 +1,29 @@
+import { NegocioErro } from "../error/NegocioErro";
+
 export class Conta {
-  public sacar(): number {
-    return 1000;
+  private _numero: string;
+  private _saldo: number;
+
+  public constructor(numero: string, saldo: number) {
+    this._numero = numero;
+    this._saldo = saldo;
+  }
+
+  public sacar(valor: number): void {
+    if (valor == 0)
+      throw new NegocioErro("valor não pode ser igual ou menor que zero");
+
+    this._saldo -= valor;
+  }
+
+  public get saldo(): number {
+    return this._saldo;
+  }
+
+  public depositar(valor: number): void {
+    if (valor == 0)
+      throw new NegocioErro("valor não pode ser igual ou menor que zero");
+
+    this._saldo += valor;
   }
 }
