e9a5c0710217e68140dcd21e12bbff557ba42700
Diego Santos Dev <zerbiniprogramador@gmail.com>
2026-01-20 22:48:02 -0300

    feat: integration and automated tests

---
+++ web/__tests__/pages/api/contas/index.test.ts
@@ -0,0 +1,45 @@
+import { createMocks } from "node-mocks-http";
+import contas from "../../../../pages/api/contas/index";
+import { prismaMock } from "../../../mock/prisma";
+
+describe("API criação de contas", () => {
+    test("criação com sucesso", async() => {
+        const { req, res } = createMocks({
+            method: "POST",
+            url: "api/contas/",
+            body: {
+                numero: "123456",
+                saldo: 100.0
+            }
+        });
+
+        await contas(req, res);
+
+        expect(res.statusCode).toBe(201);
+        expect(res._getJSONData()).toMatchObject(
+            expect.objectContaining({
+                mensagem: "sucesso"
+            })
+        );
+    });
+
+    test("criação sem sucesso", async() => {
+        const { req, res } = createMocks({
+            method: "POST",
+            url: "api/contas/",
+            body: {
+                numero: "1234567",
+                saldo: 100.0
+            }
+        });
+
+        await contas(req, res);
+        
+        expect(res.statusCode).toBe(400);
+        expect(res._getJSONData()).toMatchObject(
+            expect.objectContaining({
+                mensagem: "número de conta inválida"
+            })
+        );
+    });
+});
